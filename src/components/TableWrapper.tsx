993fe39a5c24475312591e476576c399aac7f361
BenAlaa <ahmed.alaa0794@gmail.com>
2022-05-02 22:43:35 +0200

    create page and table wrapper

---
+++ src/components/TableWrapper.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+
+const TableWrapper = styled.section`
+  width: 80%;
+  display: flex;
+  justify-content: center;
+  margin-top: 3rem;
+`;
+
+export default TableWrapper;
